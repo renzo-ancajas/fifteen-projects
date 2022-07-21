813f373cb9e7818378c9ce74eab5cfe895e7f185
Raphael <raphaelaquino23@gmail.com>
2022-07-21 16:14:44 +0800

    add styles and lis

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,24 +17,68 @@ const Navbar = () => {
         <ul className="links">
           <li>
             <a href="#">Birthday</a>
+          </li>
+          <li>
             <a href="#">Tours</a>
+          </li>
+          <li>
             <a href="#">Reviews</a>
+          </li>
+          <li>
             <a href="#">Accordion</a>
+          </li>
+          <li>
             <a href="#">Menu</a>
+          </li>
+          <li>            
             <a href="#">Tabs</a>
+          </li>
+          <li>
             <a href="#">Slider</a>
+          </li>
+          <li>
             <a href="#">Lorem Ipsum</a>
+          </li>
+          <li>
             <a href="#">Grocery Bud</a>
+          </li>
+          <li>
             <a href="#">Navbar</a>
+          </li>
+          <li>
             <a href="#">Sidebar</a>
+          </li>
+          <li>
             <a href="#">Stripes</a>
+          </li>
+          <li>
             <a href="#">Cart</a>
+          </li>
+          <li>
             <a href="#">Cocktail</a>
+          </li>
+          <li>
             <a href="#">App</a>
           </li>
         </ul>
       </div>
-      <ul className="social-logins"></ul>
+      <ul className="social-logins">
+        <li>
+          <a href="https://www.twitter.com">
+            <FaTwitter/>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com">
+            <FaTwitter/>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com">
+            <FaTwitter/>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 }
