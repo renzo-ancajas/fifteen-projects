83fd2cad34bbdb95215c290fdae8eff75bb9b819
Raphael <raphaelaquino23@gmail.com>
2022-07-21 16:38:30 +0800

    finish navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,86 +1,58 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { FaBars, FaTwitter } from 'react-icons/fa';
-//import { links, social } from './data';
-import logo from '../logo.png';
-import "../Styles/Navbar.css"
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "../Data/navbarData";
+import logo from "../logo.png";
+import "../Styles/Navbar.css";
 
 const Navbar = () => {
-  return <nav>
-    <div className="nav-center">
-      <div className="nav-header">
-        <img className="logo" src={logo} alt="logo" />
-        <button className="nav-toggle">
-          <FaBars />
-        </button>
-      </div>
-      <div className="links-container show-container">
-        <ul className="links">
-          <li>
-            <a href="#">Birthday</a>
-          </li>
-          <li>
-            <a href="#">Tours</a>
-          </li>
-          <li>
-            <a href="#">Reviews</a>
-          </li>
-          <li>
-            <a href="#">Accordion</a>
-          </li>
-          <li>
-            <a href="#">Menu</a>
-          </li>
-          <li>            
-            <a href="#">Tabs</a>
-          </li>
-          <li>
-            <a href="#">Slider</a>
-          </li>
-          <li>
-            <a href="#">Lorem Ipsum</a>
-          </li>
-          <li>
-            <a href="#">Grocery Bud</a>
-          </li>
-          <li>
-            <a href="#">Navbar</a>
-          </li>
-          <li>
-            <a href="#">Sidebar</a>
-          </li>
-          <li>
-            <a href="#">Stripes</a>
-          </li>
-          <li>
-            <a href="#">Cart</a>
-          </li>
-          <li>
-            <a href="#">Cocktail</a>
-          </li>
-          <li>
-            <a href="#">App</a>
-          </li>
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <ul className="social-logins">
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-}
+    </nav>
+  );
+};
 
 export default Navbar;
---
+++ src/Data/navbarData.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+export const links = [
+  {
+    id: 1,
+    url: '/Birthday',
+    text: 'Birthday',
+  },
+  {
+    id: 2,
+    url: '/Tours',
+    text: 'Tours',
+  },
+  {
+    id: 3,
+    url: '/Reviews',
+    text: 'Reviews',
+  },
+  {
+    id: 4,
+    url: '/Accordion',
+    text: 'Accordion',
+  },
+  {
+    id: 5,
+    url: '/Menu',
+    text: 'Menu',
+  },
+  {
+    id: 6,
+    url: '/Tabs',
+    text: 'Tabs',
+  },
+  {
+    id: 7,
+    url: '/Slider',
+    text: 'Slider',
+  },
+  {
+    id: 8,
+    url: '/LoremIpsum',
+    text: 'Lorem Ipsum',
+  },
+  {
+    id: 9,
+    url: '/ColorGenerator',
+    text: 'Color Generator',
+  },
+  {
+    id: 10,
+    url: '/GroceryBud',
+    text: 'Grocery Bud',
+  },
+  {
+    id: 11,
+    url: '/Stripes',
+    text: 'Stripes',
+  },
+  {
+    id: 12,
+    url: '/Cart',
+    text: 'Cart',
+  },
+  {
+    id: 13,
+    url: '/Cocktail',
+    text: 'Cocktail',
+  },
+];
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.behance.net',
+    icon: <FaBehance />,
+  },
+];
